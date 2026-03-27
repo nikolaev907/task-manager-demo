ab0e35e41d02997ae8418d587e15e8aef9d82492
Alexey  Nikolaev <nikolaev1972.72@mail.ru>
2026-03-27 19:11:35 +0300

    refactor: add bean taskNotificationTopic
    Created topic in KafkaProducerConfig.java

--- backend/src/main/java/com/demo/taskmanager/kafka/KafkaProducerConfig.java
+++ backend/src/main/java/com/demo/taskmanager/kafka/KafkaProducerConfig.java
@@ -1,10 +1,12 @@
 package com.demo.taskmanager.kafka;
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
@@ -36,5 +38,13 @@ public class KafkaProducerConfig {
     @Bean
     public KafkaTemplate<String, Object> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
+    }
+
+    @Bean
+    public NewTopic taskNotificationTopic() {
+        return TopicBuilder.name("task-notifications")
+                .partitions(1)
+                .replicas(1)
+                .build();
     }
 }
