70f5959c0f95c19a53ee4b12ec11fd042141cc33
Alexey  Nikolaev <nikolaev1972.72@mail.ru>
2026-03-27 18:50:40 +0300

    refactor: add field
    Added updateAt field to Notification.java

--- backend/src/main/java/com/demo/taskmanager/model/Notification.java
+++ backend/src/main/java/com/demo/taskmanager/model/Notification.java
@@ -1,19 +1,25 @@
 package com.demo.taskmanager.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Notification {
     private String message;
     private String taskTitle;
     private TaskStatus status;
     private LocalDateTime timestamp = LocalDateTime.now();
+    private LocalDateTime updateAt;
 
     public Notification(String message, String taskTitle, TaskStatus status, LocalDateTime updateAt) {
         this.message = message;
         this.taskTitle = taskTitle;
         this.status = status;
+        this.updateAt = updateAt;
     }
 }
